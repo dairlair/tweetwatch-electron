d3fdb2a9b5373d7ce97e0238ffbdcf2ebc789344
dairlair <51401608+dairlair@users.noreply.github.com>
2019-07-21 11:54:23 +0300

    Basic navigation implemented with ant design and react router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Menu, Icon } from 'antd';
+const { SubMenu } = Menu;
+
+class Header extends React.Component {
+    state = {
+      current: 'mail',
+    };
+  
+    handleClick = (e: { key: any; }) => {
+      console.log('click ', e);
+      this.setState({
+        current: e.key,
+      });
+    };
+  
+    render() {
+      return (
+        <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+          <Menu.Item key="dashboard">
+            Dashboard
+            <Link to="/" />
+          </Menu.Item>
+          <Menu.Item key="subjects">
+            Subjects
+            <Link to="/subjects" />
+          </Menu.Item>
+        </Menu>
+      );
+    }
+  }
 
 // The Header creates links that can be used to navigate
 // between routes.
-const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/subjects'>Subjects</Link></li>
-      </ul>
-    </nav>
-  </header>
-)
+// const Header = () => (
+//   <header>
+//     <nav>
+//       <ul>
+//         <li><Link to='/'>Home</Link></li>
+//         <li><Link to='/subjects'>Subjects</Link></li>
+//       </ul>
+//     </nav>
+//   </header>
+// )
 
 export default Header
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import "antd/dist/antd.css";
+import './index.css';
 
 render((
     <BrowserRouter>
