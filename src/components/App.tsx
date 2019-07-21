b5a0e5a0e4ed14b6283796ee3381557e6260836a
dairlair <51401608+dairlair@users.noreply.github.com>
2019-07-21 11:34:14 +0300

    Simple react routing added.

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Main from './Main'
+
+const App: React.FC = () => {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+      </div>
+    );
+}
+
+
+export default App
---
+++ src/components/Subjects.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'antd/es/button';
+
+class Subjects extends React.Component {
+  render() {
+    return <div>
+      <h1>Subjects</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  }
+}
+
+export default Subjects
