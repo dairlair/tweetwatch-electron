507f50cdbfcc59add8bdc8ac816f6ac1d16c9129
dairlair <dairlair@my.com>
2019-10-03 20:48:47 +0300

    Restore

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignupForm from '../components/SignupForm'
+
+const Signup = () => (
+  <div>
+    <h1>Sign up</h1>
+    <SignupForm />
+  </div>
+)
+
+export default Signup
